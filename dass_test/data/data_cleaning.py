567d10b9bde328018fcc43f229a997bd45db7a62
Yuqi Feng <fengyuqi621@gmail.com>
2020-09-30 21:09:37 -0700

    drop columns

--- dass_test/data/data_cleaning.py
+++ dass_test/data/data_cleaning.py
@@ -1,15 +1,6 @@
 import pandas as pd
 
-fields = ["Q1A","Q1I","Q1E","Q2A","Q2I","Q2E","Q3A","Q3I","Q3E","Q4A","Q4I","Q4E","Q5A","Q5I","Q5E","Q6A","Q6I","Q6E","Q7A","Q7I",
-"Q7E","Q8A","Q8I","Q8E","Q9A","Q9I","Q9E","Q10A","Q10I","Q10E","Q11A","Q11I","Q11E","Q12A","Q12I","Q12E","Q13A","Q13I","Q13E","Q14A",
-"Q14I","Q14E","Q15A","Q15I","Q15E","Q16A","Q16I","Q16E","Q17A","Q17I","Q17E","Q18A","Q18I","Q18E","Q19A","Q19I","Q19E","Q20A","Q20I",
-"Q20E","Q21A","Q21I","Q21E","Q22A","Q22I","Q22E","Q23A","Q23I","Q23E","Q24A","Q24I","Q24E","Q25A","Q25I","Q25E","Q26A","Q26I","Q26E",
-"Q27A","Q27I","Q27E","Q28A","Q28I","Q28E","Q29A","Q29I","Q29E","Q30A","Q30I","Q30E","Q31A","Q31I","Q31E","Q32A","Q32I","Q32E","Q33A",
-"Q33I","Q33E","Q34A","Q34I","Q34E","Q35A","Q35I","Q35E","Q36A","Q36I","Q36E","Q37A","Q37I","Q37E","Q38A","Q38I","Q38E","Q39A","Q39I",
-"Q39E","Q40A","Q40I","Q40E","Q41A","Q41I","Q41E","Q42A","Q42I","Q42E","country","source","introelapse","testelapse","surveyelapse",
-"TIPI1","TIPI2","TIPI3","TIPI4","TIPI5","TIPI6","TIPI7","TIPI8","TIPI9","TIPI10","VCL1","VCL2","VCL3","VCL4","VCL5","VCL6","VCL7","VCL8",
-"VCL9","VCL10","VCL11","VCL12","VCL13","VCL14","VCL15","VCL16","education","urban","gender","engnat","age","screensize",
-"uniquenetworklocation","hand","religion","orientation","race","voted","married","familysize","major"]
+
 
 # Explanation for the parameters
 # skiprows:  skip the number of rows at the start of the csv file. In this case, we want to skip the 1st row (the header)
@@ -26,19 +17,46 @@ fields = ["Q1A","Q1I","Q1E","Q2A","Q2I","Q2E","Q3A","Q3I","Q3E","Q4A","Q4I","Q4E
 # Ian (use the following absolute path to read in the csv & comment the above line of code)
 
 
+def drop_columns (df):
+    # data cleaning
+    # Step 1: Drop columns
+    # Step 1.1: drop "engnat", "screensize", "uniquenetworklocation", "introelapse", "surveyelapse"
+    cleaned_table = df.drop(columns=["engnat", "screensize", "uniquenetworklocation", "introelapse", "surveyelapse"])
+
+    # Step 1.2: drop all the time and position columns
+    for i in range(1, 43):
+        positionColumn, timeColumn = "Q{i}I".format(i=i), "Q{i}E".format(i=i)
+        cleaned_table = cleaned_table.drop(columns=[positionColumn, timeColumn])
+    return cleaned_table
+
 def drop_rows (df):
+    # Step 2: Eliminate the rows that do not pass the validity check
+    # Step 2.1: Select all the columns that contain the string "VCL"
+    vcl_list = df.columns[df.columns.str.contains(pat = 'VCL')].tolist()
+    # for each_vcl in vcl_list:
+    #     sum_vcl =  sum_vcl + df[each_vcl]
+    # df['VCLTotal'] = sum_vcl
+    # print(df['VCLTotal'])
 
 
-
-    
 def main():
-    df = pd.read_csv('/Users/ianyu/Desktop/intothedata/dass_test/data/dass_data.csv', 
+    fields = ["Q1A","Q1I","Q1E","Q2A","Q2I","Q2E","Q3A","Q3I","Q3E","Q4A","Q4I","Q4E","Q5A","Q5I","Q5E","Q6A","Q6I","Q6E","Q7A","Q7I",
+"Q7E","Q8A","Q8I","Q8E","Q9A","Q9I","Q9E","Q10A","Q10I","Q10E","Q11A","Q11I","Q11E","Q12A","Q12I","Q12E","Q13A","Q13I","Q13E","Q14A",
+"Q14I","Q14E","Q15A","Q15I","Q15E","Q16A","Q16I","Q16E","Q17A","Q17I","Q17E","Q18A","Q18I","Q18E","Q19A","Q19I","Q19E","Q20A","Q20I",
+"Q20E","Q21A","Q21I","Q21E","Q22A","Q22I","Q22E","Q23A","Q23I","Q23E","Q24A","Q24I","Q24E","Q25A","Q25I","Q25E","Q26A","Q26I","Q26E",
+"Q27A","Q27I","Q27E","Q28A","Q28I","Q28E","Q29A","Q29I","Q29E","Q30A","Q30I","Q30E","Q31A","Q31I","Q31E","Q32A","Q32I","Q32E","Q33A",
+"Q33I","Q33E","Q34A","Q34I","Q34E","Q35A","Q35I","Q35E","Q36A","Q36I","Q36E","Q37A","Q37I","Q37E","Q38A","Q38I","Q38E","Q39A","Q39I",
+"Q39E","Q40A","Q40I","Q40E","Q41A","Q41I","Q41E","Q42A","Q42I","Q42E","country","source","introelapse","testelapse","surveyelapse",
+"TIPI1","TIPI2","TIPI3","TIPI4","TIPI5","TIPI6","TIPI7","TIPI8","TIPI9","TIPI10","VCL1","VCL2","VCL3","VCL4","VCL5","VCL6","VCL7","VCL8",
+"VCL9","VCL10","VCL11","VCL12","VCL13","VCL14","VCL15","VCL16","education","urban","gender","engnat","age","screensize",
+"uniquenetworklocation","hand","religion","orientation","race","voted","married","familysize","major"]
+    df = pd.read_csv('/Users/fengyuqi/Desktop/intothedata/dass_test/data/dass_data.csv', 
     sep=',', skiprows=1, header=None, engine='python', names=fields)
-    sns.set()
 
+    cleaned_table = drop_columns(df)
+   
     drop_rows(df)
-
-
+    # cleaned_data = drop_rows(cleaned_table)
 
 if __name__ == '__main__':
     main()
